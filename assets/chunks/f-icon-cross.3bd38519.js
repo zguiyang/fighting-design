866e72f8e7f72a9537ccaff3a1524d7e3ea246c6
zguiyang <zguiyang@users.noreply.github.com>
2022-10-20 03:20:13 +0000

    deploy: 8377d41468325a5f9f73b5cdddbc7ce38723d323

---
+++ assets/chunks/f-icon-cross.3bd38519.js
@@ -0,0 +1,1 @@
+import{o as e,c as o,d as t}from"../app.d762b748.js";const n={width:"1em",height:"1em",viewBox:"0 0 16 16"},r=t("path",{fill:"none",stroke:"currentColor","stroke-linecap":"round","stroke-linejoin":"round","stroke-width":"1.5",d:"m11.25 4.75-6.5 6.5m0-6.5 6.5 6.5"},null,-1),s=[r];function c(i,d){return e(),o("svg",n,s)}const l={render:c};export{l as F};
