f0071ae82d7314df180277354f8b7afb8de9aab3
Tyh2001 <Tyh2001@users.noreply.github.com>
2022-10-20 12:33:57 +0000

    deploy: f7122519137107123d310d0a648b8cd45c29df3c

--- assets/chunks/copy.998cefc6.js
+++
@@ -1,1 +0,0 @@
-import{l as t}from"../app.d762b748.js";const n=o=>{const e=document.createElement("input");e.value=o,document.body.appendChild(e),e.select(),document.execCommand("Copy"),document.body.removeChild(e),t({message:"\u590D\u5236\u6210\u529F",type:"primary"})};export{n as o};
