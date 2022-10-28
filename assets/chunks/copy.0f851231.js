b05479af1d042205ecb75d0e3febe71ef7acb035
zguiyang <zguiyang@users.noreply.github.com>
2022-10-28 02:08:44 +0000

    deploy: 699b70b18fe02145315020c5f099322091ef1d92

---
+++ assets/chunks/copy.0f851231.js
@@ -0,0 +1,1 @@
+import{m as t}from"../app.39674fd0.js";const n=o=>{const e=document.createElement("input");e.value=o,document.body.appendChild(e),e.select(),document.execCommand("Copy"),document.body.removeChild(e),t({message:"\u590D\u5236\u6210\u529F",type:"primary"})};export{n as o};
--- assets/chunks/copy.d86d4722.js
+++
@@ -1,1 +0,0 @@
-import{l as t}from"../app.09f743d7.js";const n=o=>{const e=document.createElement("input");e.value=o,document.body.appendChild(e),e.select(),document.execCommand("Copy"),document.body.removeChild(e),t({message:"\u590D\u5236\u6210\u529F",type:"primary"})};export{n as o};
